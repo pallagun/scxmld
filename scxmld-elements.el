cbfa99c63f751020c8a8f60de1c69347fde5c46e
pallagun <pallagun@no.email>
2020-02-29 07:54:46 -0500

    cleaning out old comments

--- scxmld-elements.el
+++ scxmld-elements.el
@@ -116,33 +116,5 @@ Special cases here are: id, initial."
            (scxml-put-attrib element attribute-name attribute-value)
          (scxml-delete-attrib element attribute-name)))))
 
-
-
-
-;; (defun scxmld-element-factory (type attrib-alist)
-;;   "Build a drawable element of TYPE and having ATTRIB-ALIST properties."
-;;   (let* ((base-xml-element-name (symbol-name type))
-;;          (base-class (intern (format "scxmld-%s" base-xml-element-name)))
-;;          (base-slots (eieio-class-slots base-class))
-;;          ;; TODO - Is there something more appropriate than a cl--* function?
-;;          (base-slot-symbols (mapcar 'cl--slot-descriptor-name base-slots))
-;;          (drawable-class (intern (format "scxml-drawable-%s" base-xml-element-name)))
-;;          (drawable-slots (eieio-class-slots drawable-class))
-;;          (drawable-slot-symbols (mapcar 'cl--slot-descriptor-name drawable-slots))
-;;          (skip-slots))
-;;     ;; Build a list of all drawable-slots that aren't in teh base slots and
-;;     ;; use them as as a skip-slots list.
-;;     (mapc (lambda (slot-sym)
-;;             (when (not (memq slot-sym base-slot-symbols))
-;;               (push slot-sym skip-slots)))
-;;           drawable-slot-symbols)
-;;     (let ((element (scxml--element-factory (intern (format "drawable-%s" base-xml-element-name))
-;;                                            attrib-alist
-;;                                            skip-slots)))
-;;       (scxml--set-hint-from-attrib-list element attrib-alist)
-;;       (scxml--build-synthetic-children element attrib-alist)
-;;       element)))
-
-
 (provide 'scxmld-elements)
 ;;; scxmld-elements.el ends here
